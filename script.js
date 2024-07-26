638902d264a60b4b0af33c81ac0a74676e33f20c
bugDje <bugdje@gmail.com>
2024-07-26 19:41:02 +0200

    revoir le positionnement footer

--- script.js
+++ script.js
@@ -3,31 +3,26 @@ const quoteText = document.querySelector(".quote"),
   resultBtn = document.querySelector(".box-reponse"),
   resultQuote = document.querySelector(".reponse"),
   auteurQuote = document.querySelector(".auteur .nom"),
-  copyBtn = document.querySelector(".copy"), 
+  copyBtn = document.querySelector(".copy"),
   copyText = document.querySelector(".copy_text");
 
 quoteBtn.addEventListener("click", randomQuote);
 
-copyBtn.addEventListener("click", ()=>{
-navigator.clipboard.writeText(quoteText.innerText + "  Réponse : " +  resultQuote.innerText);
-copyText.innerText = "Blague copié";
-copyText.style.opacity = "0.7";
-});
-
+randomQuote();
 
 function randomQuote() {
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading..";
   copyText.innerText = "Copier la Blague";
-copyText.style.opacity = "1";
+  copyText.style.opacity = "1";
   resultQuote.style.visibility = "collapse";
   fetch("https://api.blablagues.net/?rub=blagues")
     .then((res) => res.json())
     .then((result) => {
-      console.log(result);
-      quoteText.innerHTML = `${result.data.content.text_head}<br>${result.data.content.text}`;
+      quoteText.innerHTML = `${
+        result.data.content.text_head
+      }<br>${result.data.content.text.replace(/\n/gi, `<br>`)}`;
       auteurQuote.innerHTML = `${result.data.author.pseudo}`;
-    
 
       resultBtn.addEventListener("click", clicReponse);
       function clicReponse() {
@@ -35,14 +30,28 @@ copyText.style.opacity = "1";
         resultBtn.style.visibility = "hidden";
         resultQuote.style.visibility = "visible";
       }
+      copyBtn.addEventListener("click", () => {
+        if (result.data.content.text_hidden === "") {
+          navigator.clipboard.writeText(
+            quoteText.innerText + resultQuote.innerText
+          );
+          copyText.innerText = "Blague copié";
+          copyText.style.opacity = "0.7";
+        } else {
+          navigator.clipboard.writeText(
+            quoteText.innerText + "  Réponse : " + resultQuote.innerText
+          );
+          copyText.innerText = "Blague copié";
+          copyText.style.opacity = "0.7";
+        }
+      });
 
       if (result.data.content.text_hidden === "") {
-        console.log("ok pour le if");
         resultBtn.style.visibility = "hidden";
       } else {
         resultBtn.style.visibility = "visible";
-        console.log("ok pour le else");
       }
+
       quoteBtn.innerText = "Autre Blague";
       quoteBtn.classList.remove("loading");
     });
